b02f310c1dec1a143c7768e6870ecf7459d1bb1e
Benjamin Hatscher <113440336+VVSBenni@users.noreply.github.com>
2024-04-26 08:34:34 +0200

    Added DisclaimerScreen

---
+++ app/Screens/DisclaimerScreen.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export default function DisclaimerScreen() {
+  return (
+    <View>
+      <Text>DisclaimerScreen</Text>
+    </View>
+  );
+}
